0eaf79d14c4c67aa0f3b1ac320a2d366b05ec303
FEDYCHUNG <fedychung@gmail.com>
2023-02-01 11:29:47 +0700

    Synk Test

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,30 +48,32 @@ export default function Home(props: HomeProps) {
         </Head>
 
         <main className={styles.main}>
-          <Banner buttonText="Lihat caffee terdekat" handleOnClick={handleOnBannerBtnClick} />
+          <a href="https://snyk.io/test/github/FEDYCHUNG/discovery-coffee-stores.git">
 
-          <div className={styles.heroImage}>
-            <Image src="/images/hero-image.png" width={700} height={400} alt="hero" />
-          </div>
-          {coffeeStores.length > 0 && (
-            <>
-              <h2 className={styles.heading2}>Pontianak Store</h2>
-              <div className={styles.cardLayout}>
-                {coffeeStores?.map((coffeeStore) => (
-                  <div className={styles.card} key={coffeeStore.fsq_id}>
-                    <Card
-                      name={coffeeStore.name}
-                      imgUrl={
-                        coffeeStore.imgUrl ||
-                        "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
-                      }
-                      href={`/coffee-store/${coffeeStore.fsq_id}`}
-                    />
-                  </div>
-                ))}
-              </div>
-            </>
-          )}
+            <Banner buttonText="Lihat caffee terdekat" handleOnClick={handleOnBannerBtnClick} />
+
+            <div className={styles.heroImage}>
+              <Image src="/images/hero-image.png" width={700} height={400} alt="hero" />
+            </div>
+            {coffeeStores.length > 0 && (
+              <>
+                <h2 className={styles.heading2}>Pontianak Store</h2>
+                <div className={styles.cardLayout}>
+                  {coffeeStores?.map((coffeeStore) => (
+                    <div className={styles.card} key={coffeeStore.fsq_id}>
+                      <Card
+                        name={coffeeStore.name}
+                        imgUrl={
+                          coffeeStore.imgUrl ||
+                          "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                        }
+                        href={`/coffee-store/${coffeeStore.fsq_id}`}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </>
+            )}
         </main>
       </div>
     );
